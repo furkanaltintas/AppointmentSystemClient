f5b71936400d81c24b76a6eba680e43b0e62bacf
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-03-23 04:00:08 +0300

    ✨ Constants oluşturuldu.

---
+++ src/app/constants/urlconstants.ts
@@ -0,0 +1,19 @@
+export const api: string ="https://localhost:7135/api/";
+export const claims: string = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims";
+export const tokenKey: string = "token";
+
+// Client
+export const login: string = "/login";
+
+
+
+// Backend
+export const loginAction: string = "Auth/Login";
+
+export const doctorAction: string = "Doctors/GetAll";
+export const doctorActionCreate: string = "Doctors/Create";
+export const doctorActionDelete: string = "Doctors/Delete";
+
+export const departmentAction: string = "Departments/GetAll";
+export const departmentActionCreate: string = "Departments/Create";
+export const departmentActionDelete: string = "Departments/Delete";
